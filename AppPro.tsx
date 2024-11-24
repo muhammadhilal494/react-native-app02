5bf707d9f17bc446954d0fc4b31a8609e03bfa15
Muhammad Hilal <muhammadhilal494513@gmail.com>
2024-11-24 21:07:29 +0500

    Styling React Native Components

---
+++ AppPro.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme
+} from 'react-native';
+
+function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+                Hello World!
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Make the container fill the screen
+        justifyContent: 'center', // Push content to the bottom
+        alignItems: 'center', // Align content to the right
+        backgroundColor: '#f8f8f8', // Light gray background color
+    },
+    whiteText: {
+        color: '#FFFFFF',
+        fontSize: 18, // Optional: Make text more visible
+    },
+    darkText: {
+        color: '#000000',
+        fontSize: 18, // Optional: Make text more visible
+    },
+});
+
+export default AppPro;
